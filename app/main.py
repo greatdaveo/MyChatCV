143d54f4a6982130a49479828abd639efe9842b0
David Olowomeye <olowomeyedavid242@gmail.com>
2025-05-27 06:56:42 +0100

    Added WhatsApp CV route and message parsing logic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.api import whatsapp
+from app.routes import whatsapp
 
 app = FastAPI(title="MyChatCV API")
 
 # To include WhatsApp bot route
-# app.include_router(whatsapp.router, prefix="/api/whatsapp")
+app.include_router(whatsapp.router, prefix="/api/whatsapp")
 
 @app.get("/")
 def read_root():
